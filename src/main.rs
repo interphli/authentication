8b1b56ed928549906bf0029072732ad3970889d0
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-23 14:24:22 +0300

    initial module setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::error::Error as StdError;
+
+
+mod config;
+mod domain;
+mod server;
+
+
+pub type Result<T> = std::result::Result<T, Box<dyn StdError>>;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Hello, world!");
+    Ok(())
 }
