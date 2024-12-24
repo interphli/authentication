e940ba1c1b724c356fe77143563db7112a06956f
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-24 14:50:42 +0300

    created the id module and the Id type

---
+++ src/domain/types/id.rs
@@ -0,0 +1,71 @@
+use serde::{Serialize, Deserialize, Serializer};
+use aws_sdk_dynamodb::types::AttributeValue;
+use std::ops::{Deref, DerefMut};
+use bson::oid::ObjectId;
+use std::str::FromStr;
+use crate::{Result, StdError};
+
+
+#[derive(Clone, Debug, Deserialize, Default)]
+pub struct Id(ObjectId);
+
+
+
+impl Deref for Id {
+    type Target = ObjectId;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Id {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+
+impl FromStr for Id {
+    type Err = Box<dyn std::error::Error>;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Ok(Id(ObjectId::from_str(s)?))
+    }
+}
+
+
+impl Serialize for Id {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+        where
+            S: Serializer {
+        serializer.serialize_str(&self.0.to_hex())
+    }
+}
+
+
+impl From<Id> for AttributeValue {
+    fn from(id: Id) -> Self {
+        let bytes = id.bytes();
+        AttributeValue::B(bytes.as_ref().into())
+    }
+}
+
+impl TryFrom<AttributeValue> for Id {
+    type Error = Box<dyn StdError>;
+    fn try_from(value: AttributeValue) -> Result<Self> {
+        match value {
+            AttributeValue::B(blob) => {
+                let bytes = blob.into_inner();
+                let array: [u8; 12] = bytes.as_slice().try_into().map_err(|_|"invalid id length")?;
+                let oid = array.into();
+                Ok(Id(oid))
+            },
+            AttributeValue::S(string) => {
+                let bytes = Vec::from(string);
+                let array: [u8; 12] = bytes.as_slice().try_into().map_err(|_|"invalid id length")?;
+                let oid = array.into();
+                Ok(Id(oid))
+            },
+            _ => Err("invalid id type")?
+        }
+    }
+}
--- src/domain/types/mod.rs
+++ src/domain/types/mod.rs
@@ -1,7 +1,9 @@
 #![allow(unused)]
 mod number;
 mod value;
+mod id;
 
 
 pub use number::*;
 pub use value::*;
+pub use id::*;
