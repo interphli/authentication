e86bb0f1d4c2ce1447a635b867f1097a772ac629
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-30 10:53:05 +0300

    changed our Error from a trait  to an enum

--- src/domain/types/error.rs
+++ src/domain/types/error.rs
@@ -1,26 +1,49 @@
+use aws_sdk_dynamodb::operation::put_item::PutItemError;
+use std::fmt::{Display, Formatter, Debug};
+use std::error::Error as StdErrorTrait;
 use aws_sdk_config::error::SdkError;
 use lambda_http::http::StatusCode;
-use lambda_http::Response;
 
+pub type StdError = Box<dyn StdErrorTrait>;
 
-pub trait ResponseError {
-    type Body;
-    fn response(&self) -> Response<Self::Body>;
+#[derive(Debug)]
+pub enum Error {
+    UserNotFound,
+    VerificationCodeNotFound,
+    UserWithEmailAlreadyExists,
+    InternalServerError(StdError),
+    Custom(StatusCode, StdError)
 }
 
 
-
-impl<E: ResponseError<Body = String>, R> ResponseError for SdkError<E, R> {
-    type Body = String;
-    fn response(&self) -> Response<String> {
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            SdkError::ServiceError(err) => err.err().response(),
-            _ => {
-                let mut res = Response::new(String::new());
-                let status = res.status_mut();
-                *status = StatusCode::INTERNAL_SERVER_ERROR;
-                res
-            }
+            Error::UserNotFound => write!(f, "user not found"),
+            Error::VerificationCodeNotFound => write!(f, "verification code not found"),
+            Error::UserWithEmailAlreadyExists => write!(f, "user with the same email exists"),
+            Error::InternalServerError(err) => write!(f, "{err}"),
+            Error::Custom(status, err) => write!(f, "{err}"),
         }
+    }
+}
+
+
+impl std::error::Error for Error {}
+
+
+impl<E: Into<Error> + std::error::Error + 'static, R: Debug + 'static> From<SdkError<E, R>> for Error {
+    fn from(value: SdkError<E, R>) -> Self {
+        match value {
+            SdkError::ServiceError(err) => err.into_err().into(),
+            _ => Error::InternalServerError(Box::new(value))
+        }
+    }
+}
+
+
+impl From<PutItemError> for Error {
+    fn from(value: PutItemError) -> Self {
+       Error::InternalServerError(Box::new(value))
     }
 }
