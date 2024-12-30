5e005f2230fb1cb3e8bb129d45775f8d5d3e9073
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-30 17:01:58 +0300

    created the Either type

---
+++ src/domain/types/either.rs
@@ -0,0 +1,4 @@
+pub enum Either<R, L> {
+    Right(R),
+    Left(L)
+}
--- src/domain/types/mod.rs
+++ src/domain/types/mod.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 mod verification;
 mod number;
+mod either;
 mod value;
 mod error;
 mod user;
@@ -9,6 +10,7 @@ mod id;
 
 pub use verification::*;
 pub use number::*;
+pub use either::*;
 pub use value::*;
 pub use error::*;
 pub use user::*;
