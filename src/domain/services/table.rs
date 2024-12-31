0a8c67d7a48f3baa64714520c5ee3ce4271a5761
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-31 09:47:38 +0300

    added the expire_item methoid to the Table trait

--- src/domain/services/table.rs
+++ src/domain/services/table.rs
@@ -6,16 +6,26 @@ use std::collections::HashMap;
 
 type Result<T> = std::result::Result<T, Error>;
 
+/// The `Table` trait provides a set of methods for interacting with a database table.
+/// It requires the implementing type to be convertible to and from a `HashMap` of `AttributeValue`.
 pub trait Table : Into<HashMap<String, AttributeValue>> + TryFrom<HashMap<String, AttributeValue>, Error = StdError> {
     type PK : Into<AttributeValue>;
     type SK: Into<AttributeValue>;
     const NAME: &'static str;
     const PK_NAME: &'static str;
-    ///This is Global Secondary Index's PK
+    /// This is the Global Secondary Index's PK.
     const SK_NAME: &'static str;
 
-    /// This function checks if an item exists in the database.
-    /// It takes a client and a key (either primary or secondary) and returns a boolean.
+    /// Checks if an item exists in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `client` - A reference to the DynamoDB client.
+    /// * `key` - The primary or secondary key of the item.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing a boolean indicating whether the item exists.
     async fn item_exists(client: &Client, key: Either<Self::PK, Self::SK>) -> Result<bool> {
         let (key_name, key_value) = match key {
             Either::Left(pk) => (Self::PK_NAME, pk.into()),
@@ -30,8 +40,16 @@ pub trait Table : Into<HashMap<String, AttributeValue>> + TryFrom<HashMap<String
         Ok(output.item.is_some())
     }
 
-    /// This function retrieves an item from the database using either the primary key or secondary key.
-    /// It returns the item if it exists.
+    /// Retrieves an item from the database using either the primary key or secondary key.
+    ///
+    /// # Arguments
+    ///
+    /// * `client` - A reference to the DynamoDB client.
+    /// * `key` - The primary or secondary key of the item.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing an `Option` with the item if found, or `None` if not found.
     async fn get_item(client: &Client, key: Either<Self::PK, Self::SK>) -> Result<Option<Self>> {
         let (key_name, key_value) = match key {
             Either::Left(pk) => (Self::PK_NAME, pk.into()),
@@ -49,7 +67,16 @@ pub trait Table : Into<HashMap<String, AttributeValue>> + TryFrom<HashMap<String
         }
     }
 
-    /// This function inserts a new item to the table.
+    /// Inserts a new item into the table.
+    ///
+    /// # Arguments
+    ///
+    /// * `client` - A reference to the DynamoDB client.
+    /// * `item` - The item to be inserted.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating the success or failure of the operation.
     async fn create_item(client: &Client, item: Self) -> Result<()> {
         let _ = client.put_item()
             .table_name(Self::NAME)
@@ -58,7 +85,17 @@ pub trait Table : Into<HashMap<String, AttributeValue>> + TryFrom<HashMap<String
         Ok(())
     }
 
-    /// This function updates an item with the provided primary key.
+    /// Updates an item with the provided primary key.
+    ///
+    /// # Arguments
+    ///
+    /// * `client` - A reference to the DynamoDB client.
+    /// * `pk` - The primary key of the item.
+    /// * `update` - A `HashMap` containing the fields to update and their new values.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the updated item.
     async fn update_item(client: &Client, pk: Self::PK, update: HashMap<String, Value>) -> Result<Self> {
         let mut builder = client.update_item()
             .table_name(Self::NAME)
@@ -78,7 +115,23 @@ pub trait Table : Into<HashMap<String, AttributeValue>> + TryFrom<HashMap<String
         }
     }
 
-    /// This function deletes an item with the provided primary key.
+
+    async fn expire_item(client: &Client, pk: Self::PK, (key, value): (impl Into<String>, Value)) -> Result<()> {
+        let value = AttributeValueUpdate::builder().value(value.into()).build();
+        let _ = client.update_item().table_name(Self::NAME).key(Self::PK_NAME, pk.into()).condition_expression(&format!("attribute_exists({})", Self::PK_NAME)).send().await?;
+        Ok(())
+    }
+
+    /// Deletes an item with the provided primary key.
+    ///
+    /// # Arguments
+    ///
+    /// * `client` - A reference to the DynamoDB client.
+    /// * `pk` - The primary key of the item.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating the success or failure of the operation.
     async fn delete_item(client: &Client, pk: Self::PK) -> Result<()> {
         let _ = client.delete_item()
             .table_name(Self::NAME)
