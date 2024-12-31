5f903d268def7cd75225ed8132a0bbc5bd31d461
abdihakim148 <abdulhakimuthman100@gmail.com>
2024-12-31 16:57:28 +0300

    create a handler to handle requests

--- src/argon-main.rs
+++ src/argon-main.rs
@@ -1,12 +1,17 @@
+use lambda_http::lambda_runtime::{service_fn, run};
 use std::error::Error as StdError;
+use argon::{handler, new_argon2};
 
 mod shared;
 mod argon;
 
 
-type Result<T> = std::result::Result<T, Box<dyn StdError>>;
+type Result<T> = std::result::Result<T, Box<dyn StdError + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let argon2 = new_argon2().await;
+    let handler = service_fn(|event|handler(event, &argon2));
+    run(handler).await?;
     Ok(())
 }
--- src/argon/mod.rs
+++ src/argon/mod.rs
@@ -1,1 +1,31 @@
+use lambda_http::lambda_runtime::{LambdaEvent, Error};
+use argon2::{Argon2, PasswordHasher, PasswordVerifier, password_hash::{SaltString, rand_core::OsRng, PasswordHash}};
+#[allow(unused)]
+use crate::shared::{Request, Result as Response};
+pub use argon::new_argon2;
+
+
 mod argon;
+
+
+pub async fn handler(event: LambdaEvent<Request>, argon2: &Argon2<'static>) -> Result<Response<Option<String>, String>, Error> {
+    match event.payload {
+        Request::Hash(password) => {
+            let salt = SaltString::generate(OsRng);
+            match argon2.hash_password(password.as_bytes(), &salt) {
+                Ok(value) => Ok(Response::Ok(Some(value.to_string()))),
+                Err(err) => Ok(Response::Err(err.to_string()))
+            }
+        },
+        Request::Verify(password, hash) => {
+            let hash = match PasswordHash::new(&hash) {
+                Ok(value) => value,
+                Err(err) => return Ok(Response::Err(err.to_string()))
+            };
+            match argon2.verify_password(password.as_bytes(), &hash) {
+                Ok(_) => Ok(Response::Ok(None)),
+                Err(err) => Ok(Response::Err(err.to_string()))
+            }
+        }
+    }
+}
