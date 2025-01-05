6c6998db42803666230056be102413389d0dd3a7
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-05 17:40:41 +0300

    modularizing the shared crate

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,25 +1,4 @@
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "server", derive(serde::Deserialize))]
-#[cfg_attr(feature = "client", derive(serde::Serialize))]
-pub enum Request {
-    Hash(String),
-    Verify(String, String)
-}
-
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "server", derive(serde::Serialize))]
-#[cfg_attr(feature = "client", derive(serde::Deserialize))]
-pub enum Result<T, E> {
-    Ok(T),
-    Err(E)
-}
+mod argon;
 
 
-impl<T, E> From<Result<T, E>> for std::result::Result<T, E> {
-    fn from(result: Result<T, E>) -> Self {
-        match result {
-            Result::Ok(value) => Ok(value),
-            Result::Err(err) => Err(err)
-        }
-    }
-}
+pub use argon::*;
