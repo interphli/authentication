f8e86cce653a21d9c8a7ddaec501a0ec4a9bae67
abdihakim148 <abdulhakimuthman100@gmail.com>
2025-01-08 10:23:21 +0300

    completely redefined the OAuthProvider struct

--- main/src/domain/types/oauthprovider.rs
+++ main/src/domain/types/oauthprovider.rs
@@ -1,6 +1,7 @@
-use oauth2::{basic::BasicClient, Scope, ClientId, ClientSecret, AuthUrl, TokenUrl};
+use oauth2::{basic::BasicClient, AuthUrl, ClientId, ClientSecret, CsrfToken, Scope, TokenUrl};
 use serde::{Serialize, Deserialize};
-use super::Error;
+use std::collections::HashMap;
+use super::{Error, Value};
 use url::Url;
 
 
@@ -13,17 +14,42 @@ pub struct OAuthProvider {
     pub scopes: Vec<Scope>,
     pub params: Vec<(String, String)>,
     pub open_id_url: Url,
+    pub renames: Vec<(String, String)>
 }
 
 
-// impl From<OAuthProvider> for (BasicClient, Vec<Scope>, Option<Vec<(String, String)>>, Url, Vec<(String, String)>) {
-//     fn from(provider: OAuthProvider) -> Self {
-//         let client_id = ClientId::new(provider.client_id);
-//         let client_secret = Some(ClientSecret::new(provider.client_secret));
-//         let auth_url = AuthUrl::from_url(provider.auth_url);
-//         let token_url = Some(TokenUrl::from_url(provider.token_url));
-//         let client = BasicClient::new(client_id, client_secret, auth_url, token_url);
-//         let scopes = provider.scopes.into_iter().map(|scope|Scope::new(scope)).collect();
-//         (client, scopes, provider.params, provider.open_id_url, provider.renames)
-//     }
-// }
+impl OAuthProvider {
+    fn authorize_url(&self) -> (Url, CsrfToken) {
+        let mut url = self.client.authorize_url(CsrfToken::new_random).add_scopes(self.scopes.clone());
+        for (name, value) in &self.params  {
+            url = url.add_extra_param(name, value);
+        }
+        url.url()
+    }
+
+    fn rename(&self, map: &mut HashMap<String, Value>) {
+        for (key, rename) in &self.renames {
+            if let Some(value) = map.remove(key) {
+                map.insert(rename.to_string(), value);
+            }
+        }
+    }
+}
+
+
+impl TryFrom<HashMap<String, Value>> for OAuthProvider {
+    type Error = Box<dyn std::error::Error>;
+    fn try_from(mut map: HashMap<String, Value>) -> std::result::Result<Self, Self::Error> {
+        let client_id = map.remove("").ok_or("err")?;
+        let client_id = ClientId::new(map.remove("client_id").ok_or("field client_id not found")?.try_into().map_err(|err|"expected a string for field client_id")?);
+        let client_secret = Some(ClientSecret::new(map.remove("client_secret").ok_or("field client_secret not found")?.try_into().map_err(|err|"expected a string for field client_secret")?));
+        let auth_url = AuthUrl::from_url(map.remove("auth_url").ok_or("field auth_url not found")?.try_into().map_err(|err|"expected a string for field auth_url")?);
+        let token_url = Some(TokenUrl::from_url(map.remove("token_url").ok_or("field token_url not found")?.try_into().map_err(|err|"expected a string for field token_url")?));
+        let client = BasicClient::new(client_id, client_secret, auth_url, token_url);
+        let scopes = map.remove("scopes").ok_or("field scopes not found")?.try_into().map_err(|err|"expected an array of strings for field scopes")?;
+        let params = Vec::<[String; 2]>::try_from(map.remove("params").ok_or("field params not found")?).map_err(|err|"expected an array of (String, String) pairs for field params")?.into_iter().map(|[key, value]|(key, value)).collect();
+        let open_id_url = map.remove("open_id_url").ok_or("field open_id_url not found")?.try_into().map_err(|err|"expected a string for field open_id_url")?;
+        let renames = Vec::<[String; 2]>::try_from(map.remove("renames").ok_or("field renames not found")?).map_err(|err|"expected an array of (String, String) pairs for field renames")?.into_iter().map(|[key, value]|(key, value)).collect();
+        Ok(Self{client, scopes, params, open_id_url, renames})
+    }
+}
